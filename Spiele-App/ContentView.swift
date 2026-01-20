c9e88f1df1cf5b89348880aaf041cf7b3741a8d8
David Orban <dpoxcode@orbanmail.de>
2026-01-20 18:16:24 +0100

    Initial Commit

---
+++ Spiele-App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Spiele-App
+//
+//  Created by David Orban on 20.01.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Spiele-App/Spiele_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Spiele_AppApp.swift
+//  Spiele-App
+//
+//  Created by David Orban on 20.01.26.
+//
+
+import SwiftUI
+
+@main
+struct Spiele_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
